8ec5fd30325437a4350f0876a908a78952c635b4
Patrick Schütze <45632494+Pattingerr@users.noreply.github.com>
2022-10-06 09:20:50 +0200

    Update etl.py
    updated docstrings

--- etl.py
+++ etl.py
@@ -6,6 +6,13 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """Process song file.
+    
+    Args:
+        cur: cusor of psycopg2.
+        filepath: song file path (json).
+    """    
+    
     # open song file
     df = pd.read_json(filepath, lines = True)
 
@@ -23,6 +30,13 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """Process log file.
+    
+    Args:
+        curL cusor of psycopg2.
+        filepath: log file path (json).
+    """
+    
     # open log file
     df = pd.read_json(filepath, lines = True)
 
@@ -66,6 +80,15 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+    """Process data.
+    
+    Args:
+        cur: cursor of psycopg2.
+        conn: connection of postgresql.
+        filepath: directory path of data.
+        func: function of processing.
+    """
+    
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
